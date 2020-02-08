df73ca68a856e2625815e6427ccc5de5b13a1dd0
Alexander Melashchenko <melalex490@gmail.com>
2020-02-08 15:22:53 +0200

    Merge Intervals

---
+++ leetcode-scala/src/test/scala/com/leetcode/sequences/MergeIntervalsSpec.scala
@@ -0,0 +1,21 @@
+package com.leetcode.sequences
+
+import com.leetcode.TestSpec
+import com.leetcode.sequences.MergeIntervals.merge
+
+class MergeIntervalsSpec extends TestSpec {
+
+  "MergeIntervals" when {
+    "input: [[1,3],[2,6],[8,10],[15,18]]" should {
+      "return [[1,6],[8,10],[15,18]]" in {
+        merge(Array(Array(1, 3), Array(2, 6), Array(8, 10), Array(15, 18))) should be(Array(Array(1, 6), Array(8, 10), Array(15, 18)).reverse)
+      }
+    }
+
+    "input: [[1,4],[4,5]]" should {
+      "return [[1,5]]" in {
+        merge(Array(Array(1, 4), Array(4, 5))) should be(Array(Array(1,5)))
+      }
+    }
+  }
+}
