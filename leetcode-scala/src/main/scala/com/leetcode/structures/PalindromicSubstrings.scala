ea6e411032e60d6d55277ec98af46aa5b63ea3ba
Alexander Melashchenko <melalex490@gmail.com>
2020-04-20 11:47:12 +0300

    Palindromic Substrings

--- leetcode-scala/src/main/scala/com/leetcode/structures/PalindromicSubstrings.scala
+++ leetcode-scala/src/main/scala/com/leetcode/structures/PalindromicSubstrings.scala
@@ -2,46 +2,59 @@ package com.leetcode.structures
 
 object PalindromicSubstrings {
 
+  def addBoundaries(string: String): String = {
+    if (string == null || string.isEmpty) {
+      return "^&"
+    }
+
+    val result = new Array[Char](string.length * 2 + 3)
+
+    result(0) = '^'
+    result(1) = '|'
+
+    var i = 2
+
+    for (char <- string) {
+      result(i) = char
+      result(i + 1) = '|'
+
+      i = i + 2
+    }
+
+    result(result.length - 2) = '|'
+    result(result.length - 1) = '$'
+
+    result.mkString
+  }
+
   def countSubstrings(s: String): Int = {
-    def isPalindromic(from: Int, until: Int): Boolean = {
-      var i = 0
+    val boundedString = addBoundaries(s)
+    val palindromes = new Array[Int](boundedString.length)
 
-      while (i < (until - from) / 2 + 1) {
-        if (s(until - i) != s(from + i)) {
-          return false
-        }
+    var center = 0
+    var right = 0
 
-        i += 1
+    var i = 1
+
+    while (i < boundedString.length - 1) {
+      val mirror = 2 * center - i
+
+      if (right > i) {
+        palindromes(i) = Math.min(right - i, palindromes(mirror))
       }
 
-      true
-    }
-
-    val indices = s.zipWithIndex
-      .groupMap(t => t._1)(t => t._2)
-    var result = 0
-
-    for ((_, value) <- indices) {
-      var i = 0
-
-      while (i < value.length) {
-        var j = value.length - 1
-
-        while (j >= i) {
-          val from = value(i)
-          val until = value(j)
-
-          if (isPalindromic(from, until)) {
-            result += 1
-          }
-
-          j -= 1
-        }
-
-        i += 1
+      while (boundedString(i + palindromes(i) + 1) == boundedString(i - palindromes(i) - 1)) {
+        palindromes(i) += 1
       }
+
+      if (i + palindromes(i) > right) {
+        center = i
+        right = i + palindromes(i)
+      }
+
+      i += 1
     }
 
-    result
+    palindromes.map(v => (v + 1) / 2).sum
   }
 }
