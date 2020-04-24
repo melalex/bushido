6124778a22fde1bd26b618c5767bbc9606e8bc20
Alexander Melashchenko <melalex490@gmail.com>
2020-04-24 23:12:56 +0300

    Clone Graph

---
+++ leetcode-scala/src/main/scala/com/leetcode/nonlinear/Node.scala
@@ -0,0 +1,31 @@
+package com.leetcode.nonlinear
+
+object Node {
+
+  def cloneGraph(graph: Node): Node = {
+    if (graph == null) {
+      return null
+    }
+
+    val nodes = collection.mutable.Map[Node, Node]()
+
+    def clone(node: Node): Node =
+      if (nodes.contains(node)) nodes(node)
+      else {
+        val copy = new Node(node.value)
+
+        nodes(node) = copy
+
+        copy.neighbors = node.neighbors.map(clone)
+
+        copy
+      }
+
+    clone(graph)
+  }
+}
+
+class Node(var _value: Int) {
+  var value: Int = _value
+  var neighbors: List[Node] = List()
+}
