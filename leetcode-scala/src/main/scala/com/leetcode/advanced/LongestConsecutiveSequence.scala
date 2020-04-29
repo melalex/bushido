611e9b84c450312887d3b55e70db01ee51f71bb1
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2020-04-29 22:02:07 +0300

    Longest Consecutive Sequence

---
+++ leetcode-scala/src/main/scala/com/leetcode/advanced/LongestConsecutiveSequence.scala
@@ -0,0 +1,21 @@
+package com.leetcode.advanced
+
+object LongestConsecutiveSequence {
+
+  def longestConsecutive(nums: Array[Int]): Int = {
+    val set = collection.mutable.Set(nums: _*)
+    var max = 0
+
+    for (num <- nums; if !set(num - 1)) {
+      var pointer = num
+
+      while (set(pointer)) {
+        pointer += 1
+      }
+
+      max = Math.max(pointer - num, max)
+    }
+
+    max
+  }
+}
---
+++ leetcode-scala/src/test/scala/com/leetcode/advanced/LongestConsecutiveSequenceSpec.scala
@@ -0,0 +1,15 @@
+package com.leetcode.advanced
+
+import com.leetcode.TestSpec
+import com.leetcode.advanced.LongestConsecutiveSequence.longestConsecutive
+
+class LongestConsecutiveSequenceSpec extends TestSpec {
+
+  "LongestConsecutiveSequence" when {
+    "input 100, 4, 200, 1, 3, 2" should {
+      "return 4" in {
+        longestConsecutive(Array(100, 4, 200, 1, 3, 2)) should be(4)
+      }
+    }
+  }
+}
