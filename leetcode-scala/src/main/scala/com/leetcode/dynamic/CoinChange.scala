9072ad0dd616fdfb4d0c941a33a7dc3458c51688
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2020-05-04 20:53:31 +0300

    BFS

--- leetcode-scala/src/main/scala/com/leetcode/dynamic/CoinChange.scala
+++ leetcode-scala/src/main/scala/com/leetcode/dynamic/CoinChange.scala
@@ -3,18 +3,31 @@ package com.leetcode.dynamic
 object CoinChange {
 
   def coinChange(coins: Array[Int], amount: Int): Int = {
-    val queue = collection.mutable.PriorityQueue[(List[Int], Int, Int)]((coins.toList, amount, 0))(Ordering.by[(List[Int], Int, Int), Int](_._3).reverse)
+    if (amount == 0) {
+      return 0
+    }
+
+    val queue = collection.mutable.PriorityQueue[(List[Int], Int, Int)]((coins.toList, amount, 1))(Ordering.by[(List[Int], Int, Int), Int](_._3).reverse)
+    val visited = collection.mutable.Set[(List[Int], Int)]()
 
     while (queue.nonEmpty) {
       val (currCoins, currAmount, acc) = queue.dequeue()
+      val restTails = currCoins.tail
 
-      if (currAmount == 0) {
+      if (currCoins.size > 1 && !visited((restTails, currAmount))) {
+        queue.addOne((restTails, currAmount, acc))
+        visited.add((restTails, currAmount))
+      }
+
+      val newAmount = currAmount - currCoins.head
+
+      if (newAmount == 0) {
         return acc
       }
 
-      if (currCoins.nonEmpty && currAmount > 0) {
-        queue.addOne((currCoins, currAmount - currCoins.head, acc + 1))
-        queue.addOne((currCoins.tail, currAmount, acc))
+      if (newAmount > 0 && !visited((currCoins, newAmount))) {
+        queue.addOne((currCoins, newAmount, acc + 1))
+        visited.add((currCoins, newAmount))
       }
     }
 
--- leetcode-scala/src/test/scala/com/leetcode/dynamic/CoinChangeSpec.scala
+++ leetcode-scala/src/test/scala/com/leetcode/dynamic/CoinChangeSpec.scala
@@ -20,13 +20,25 @@ class CoinChangeSpec extends TestSpec {
 
     "coins = [88,227,216,96,209,172,259], amount = 6928" should {
       "return 28" in {
-        coinChange(Array(88,227,216,96,209,172,259), 6928) should be(28)
+        coinChange(Array(88, 227, 216, 96, 209, 172, 259), 6928) should be(28)
       }
     }
 
     "coins = [1,2], amount = 3" should {
       "return 2" in {
-        coinChange(Array(1,2), 3) should be(2)
+        coinChange(Array(1, 2), 3) should be(2)
+      }
+    }
+
+    "coins = [227,99,328,299,42,322], amount = 9847" should {
+      "return 31" in {
+        coinChange(Array(227, 99, 328, 299, 42, 322), 9847) should be(31)
+      }
+    }
+
+    "coins = [346,29,395,188,155,109], amount = 9401" should {
+       "return 26" in {
+        coinChange(Array(346,29,395,188,155,109), 9401) should be(26)
       }
     }
   }
