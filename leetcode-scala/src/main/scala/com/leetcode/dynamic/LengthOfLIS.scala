d561b2b8db7271c9dabab913f66a893edd833c9d
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2020-05-06 01:14:31 +0300

    Longest Increasing Subsequence

--- leetcode-scala/src/main/scala/com/leetcode/dynamic/LengthOfLIS.scala
+++ leetcode-scala/src/main/scala/com/leetcode/dynamic/LengthOfLIS.scala
@@ -3,25 +3,34 @@ package com.leetcode.dynamic
 object LengthOfLIS {
 
   def lengthOfLIS(nums: Array[Int]): Int = {
-    val n = nums.length
-    val cache = new Array[Int](n)
-
-    def cacheRes(i: Int, value: Int): Int = {
-      cache(i) = value
-
-      value
+    if (nums.isEmpty) {
+      return 0
     }
 
-    def iter(prev: Int, i: Int): Int =
-      if (i >= n) 0
-      else if (prev >= nums(i)) iter(prev, i + 1)
-      else if (cache(i) != 0) Math.max(cache(i), iter(prev, i + 1))
-      else Math.max(iter(nums(i), i + 1), iter(prev, i + 1)) + 1
-
+    val n = nums.length
+    val cache = new Array[Int](n)
     var max = 0
+    var i = 0
 
-    for (i <- (0 until n).reverse) {
-      max = Math.max(cacheRes(i, iter(nums(i), i + 1) + 1), max)
+    cache(0) = 1
+
+    while (i < nums.length) {
+      var j = 0
+      val iVal = nums(i)
+      var currentMax = 0
+
+      while (j < i) {
+        if (nums(j) < iVal) {
+          currentMax = Math.max(currentMax, cache(j))
+        }
+
+        j += 1
+      }
+
+      cache(i) = currentMax + 1
+      max = Math.max(cache(i), max)
+
+      i += 1
     }
 
     max
