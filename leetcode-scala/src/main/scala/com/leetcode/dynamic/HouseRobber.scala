f71eec32d6474cefb1840e32e01f982d46832314
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2020-05-07 21:20:16 +0300

    House Robber

---
+++ leetcode-scala/src/main/scala/com/leetcode/dynamic/HouseRobber.scala
@@ -0,0 +1,19 @@
+package com.leetcode.dynamic
+
+object HouseRobber {
+
+  def rob(nums: Array[Int]): Int = {
+    var currentMax = 0;
+    var prevMax = 0;
+
+    for (i <- nums.indices) {
+      val currentHouse = nums(i);
+      val newMax = Math.max(currentMax, prevMax + currentHouse)
+
+      prevMax = currentMax;
+      currentMax = newMax;
+    }
+
+    currentMax
+  }
+}
