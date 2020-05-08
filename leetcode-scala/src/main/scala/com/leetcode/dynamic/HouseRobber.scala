8d45960667bfd20d51f707c47e63ff860150c8be
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2020-05-08 20:02:45 +0300

    House Robber 2

--- leetcode-scala/src/main/scala/com/leetcode/dynamic/HouseRobber.scala
+++ leetcode-scala/src/main/scala/com/leetcode/dynamic/HouseRobber.scala
@@ -3,17 +3,26 @@ package com.leetcode.dynamic
 object HouseRobber {
 
   def rob(nums: Array[Int]): Int = {
-    var currentMax = 0;
-    var prevMax = 0;
-
-    for (i <- nums.indices) {
-      val currentHouse = nums(i);
-      val newMax = Math.max(currentMax, prevMax + currentHouse)
-
-      prevMax = currentMax;
-      currentMax = newMax;
+    if (nums.length == 1) {
+      return nums(0)
     }
 
-    currentMax
+    def rob(from: Int, until: Int): Int = {
+      var currentMax = 0
+      var prevMax = 0
+
+      for (i <- from until until) {
+        val newMax = Math.max(currentMax, prevMax + nums(i))
+
+        prevMax = currentMax
+        currentMax = newMax
+      }
+
+      currentMax
+    }
+
+    val n = nums.length
+
+    Math.max(rob(0, n - 1), rob(1, n))
   }
 }
