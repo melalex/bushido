312601aa6eaf3c69622a451e69fd4aa43e1cad86
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2020-05-10 16:30:57 +0300

    Decode Ways

---
+++ leetcode-scala/src/test/scala/com/leetcode/dynamic/DecodeWaysSpec.scala
@@ -0,0 +1,33 @@
+package com.leetcode.dynamic
+
+import com.leetcode.TestSpec
+import com.leetcode.dynamic.DecodeWays.numDecodings
+
+class DecodeWaysSpec extends TestSpec {
+
+  "DecodeWays" when {
+    "input '12'" should {
+      "return 2" in {
+        numDecodings("12") should be(2)
+      }
+    }
+
+    "input '226'" should {
+      "return 3" in {
+        numDecodings("226") should be(3)
+      }
+    }
+
+    "input '2262'" should {
+      "return 3" in {
+        numDecodings("2262") should be(3)
+      }
+    }
+
+    "input '2222'" should {
+      "return 5" in {
+        numDecodings("2222") should be(5)
+      }
+    }
+  }
+}
