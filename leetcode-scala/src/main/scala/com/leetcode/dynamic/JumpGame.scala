27bd45681b8ea190e5affa390a51f7374912ffbd
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2020-05-13 19:17:25 +0300

    Jump Game

--- leetcode-scala/src/main/scala/com/leetcode/dynamic/JumpGame.scala
+++ leetcode-scala/src/main/scala/com/leetcode/dynamic/JumpGame.scala
@@ -3,22 +3,11 @@ package com.leetcode.dynamic
 object JumpGame {
 
   def canJump(nums: Array[Int]): Boolean = {
-    val visited = new Array[Boolean](nums.length)
-
-    visited(nums.length - 1) = true
-
-    for (i <- (nums.length - 1) to 0 by -1) {
-      var j = Math.min(i + nums(i), nums.length - 1)
-
-      while (!visited(i) && j >= i) {
-        if (visited(j)) {
-          visited(i) = true
-        }
-
-        j -= 1
-      }
+    var lastPos = nums.length - 1
+    for (i <- nums.length - 1 to 0 by -1) {
+      if (i + nums(i) >= lastPos) lastPos = i
     }
 
-    visited.head
+    lastPos == 0
   }
 }
