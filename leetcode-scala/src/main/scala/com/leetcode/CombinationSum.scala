ccf3ddb213dae5ce721953e1d7dba8aea16284ba
Alexander Melashchenko <alexander.melashchenko@gmail.com>
2023-11-30 00:48:39 +0200

    Combination Sum

--- leetcode-scala/src/main/scala/com/leetcode/CombinationSum.scala
+++ leetcode-scala/src/main/scala/com/leetcode/CombinationSum.scala
@@ -23,4 +23,26 @@ object CombinationSum {
 
     iter(0)
   }
+
+  def combinationSum(candidates: Array[Int], target: Int): List[List[Int]] = {
+    val queue = new scala.collection.mutable.Queue[Frame]()
+    var result = List[List[Int]]()
+
+    queue.enqueue(Frame(target, candidates.toList, Nil))
+
+    while (queue.nonEmpty) {
+      val Frame(remaining, candidates, acc) = queue.dequeue()
+
+      if (remaining == 0) {
+        result = acc :: result
+      } else if (candidates.nonEmpty && remaining > 0) {
+        queue.enqueue(Frame(remaining - candidates.head, candidates, candidates.head :: acc))
+        queue.enqueue(Frame(remaining, candidates.tail, acc))
+      }
+    }
+
+    result
+  }
+
+  private case class Frame(remaining: Int, candidates: List[Int], acc: List[Int])
 }
